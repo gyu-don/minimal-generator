6fa48cd37537500193f7b2aebb7ebd4d1fb20e63
gyu-don (Takumi Kato) <takumi.kt@gmail.com>
2023-04-12 12:25:14 +0900

    advanceを実装し、動くものができた

--- src/lib.rs
+++ src/lib.rs
@@ -1,12 +1,14 @@
-use std::{rc::Rc, cell::Cell, future::Future, pin::Pin, task::{Context, Poll}};
+use std::{rc::Rc, cell::RefCell, fmt::Debug, future::Future, pin::Pin, task::{Context, Poll}};
+mod waker;
 
+#[derive(Debug)]
 pub enum GeneratorState<T> {
     Yielded(T),
     Complete,
 }
 
 // yield_する側は、resumeする側に、Airlockを介してデータを受け渡す
-type Airlock<T> = Rc<Cell<Option<T>>>;
+type Airlock<T> = Rc<RefCell<Option<T>>>;
 pub struct Co<T> (Airlock<T>);
 
 impl<T> Co<T> {
@@ -20,7 +22,7 @@ impl<T> Co<T> {
     }
 }
 
-pub struct Gen<T, F: Future> {
+pub struct Gen<T, F> {
     airlock: Airlock<T>,
     future: Pin<Box<F>>,
 }
@@ -40,7 +42,22 @@ impl<T, F: Future> Gen<T, F> {
 
 // futureをpollすることでジェネレータの次の状態を得て、返す
 fn advance<T, F: Future>(future: Pin<&mut F>, airlock: &Airlock<T>) -> GeneratorState<T> {
-    unimplemented!();
+    // futureからpollをするのに、Contextが必要。Contextを作るのにWakerが必要。Wakerはほぼ何もしないWakerを用意する。
+    let waker = waker::create();
+    let mut cx = Context::from_waker(&waker);
+
+    match future.poll(&mut cx) {
+        Poll::Ready(_) => {
+            airlock.replace(None);
+            GeneratorState::Complete
+        },
+        Poll::Pending => {
+            match airlock.replace(None) {
+                Some(v) => GeneratorState::Yielded(v),
+                None => unreachable!(),
+            }
+        },
+    }
 }
 
 pub struct Barrier<'a, T>(&'a Airlock<T>);
@@ -48,9 +65,10 @@ impl<'a, T> Future for Barrier<'a, T> {
     type Output = ();
 
     fn poll(self: Pin<&mut Self>, _cx: &mut Context<'_>) -> Poll<Self::Output> {
-        match self.0.take() {
-            Some(_) => Poll::Pending,
-            None => Poll::Ready(()),
+        if self.0.as_ref().borrow().is_some() {
+            Poll::Pending
+        } else {
+            Poll::Ready(())
         }
     }
 }
